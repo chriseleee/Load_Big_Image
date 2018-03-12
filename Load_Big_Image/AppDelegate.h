e98c33835e0488bf72d58d22a1433b47ec90a589
chrise <gongjunping@chris.local>
2018-03-12 10:26:26 +0800

    Initial Commit

---
+++ Load_Big_Image/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Load_Big_Image
+//
+//  Created by chrise on 2018/3/12.
+//  Copyright © 2018年 chrise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Load_Big_Image/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Load_Big_Image
+//
+//  Created by chrise on 2018/3/12.
+//  Copyright © 2018年 chrise. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
